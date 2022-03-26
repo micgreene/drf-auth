81417b69c13465440dc59f51fd1db91a2274b135
micgreene <mgreene11111@yahoo.com>
2022-03-26 12:27:52 -0700

    Added JWT dependencies. Added default authentication classes. Edited views.py and urls.py to reflect JWT routing.

--- birds/views.py
+++ birds/views.py
@@ -2,13 +2,15 @@ from rest_framework import generics
 from .serializer import BirdSerializer
 from .models import Bird
 from .permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticated
+
 
 class BirdList(generics.ListCreateAPIView):
-    permission_classes = (IsOwnerOrReadOnly,)
+    permission_classes = (IsAuthenticated,)
     queryset = Bird.objects.all()
     serializer_class = BirdSerializer
 
 class BirdDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsOwnerOrReadOnly,)
+    permission_classes = (IsAuthenticated,)
     queryset = Bird.objects.all()
     serializer_class = BirdSerializer
--- drf_auth_project/settings.py
+++ drf_auth_project/settings.py
@@ -137,5 +137,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES' : ['rest_framework.permissions.IsAuthenticated', ]
+    'DEFAULT_PERMISSION_CLASSES' : ['rest_framework.permissions.IsAuthenticated', 
+    ],
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ]
 }
